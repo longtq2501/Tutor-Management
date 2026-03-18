14b1d4636a29d1b1a8c258089056e4df9d8f37c9
longtq2501 <longxp03@gmail.com>
2026-03-18 12:25:20 +0700

    refactor: update OAuth2 redirect URI format and remove unused Docker integration in tests

--- backend/src/test/java/com/tutor_management/backend/AbstractIntegrationTest.java
+++ backend/src/test/java/com/tutor_management/backend/AbstractIntegrationTest.java
@@ -2,45 +2,8 @@ package com.tutor_management.backend;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.DockerClientFactory;
-import org.testcontainers.containers.MySQLContainer;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
-
-    private static final boolean DOCKER_AVAILABLE = isDockerAvailable();
-
-    private static final MySQLContainer<?> mysql;
-
-    static {
-        if (DOCKER_AVAILABLE) {
-            mysql = new MySQLContainer<>("mysql:8.0");
-            mysql.start();
-        } else {
-            mysql = null;
-        }
-    }
-
-    @DynamicPropertySource
-    static void registerContainerProperties(DynamicPropertyRegistry registry) {
-        if (DOCKER_AVAILABLE && mysql != null) {
-            registry.add("spring.datasource.url", mysql::getJdbcUrl);
-            registry.add("spring.datasource.username", mysql::getUsername);
-            registry.add("spring.datasource.password", mysql::getPassword);
-            registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
-            registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQLDialect");
-        }
-    }
-
-    private static boolean isDockerAvailable() {
-        try {
-            return DockerClientFactory.instance().isDockerAvailable();
-        } catch (Throwable ignored) {
-            return false;
-        }
-    }
-
 }
